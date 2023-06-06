6fda61f3848d8082cc96a6e9d152a3b76c9e2e8d
MetaMechanists <percy.baker.0@gmail.com>
2023-06-06 23:14:35 +0100

    Cleanup modifyPermission

--- src/main/java/org/metamechanists/metalib/utils/PermissionUtils.java
+++ src/main/java/org/metamechanists/metalib/utils/PermissionUtils.java
@@ -2,7 +2,6 @@ package org.metamechanists.metalib.utils;
 
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.model.data.DataMutateResult;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.node.types.PermissionNode;
 import org.bukkit.entity.Player;
@@ -12,12 +11,12 @@ public class PermissionUtils {
     private static final LuckPerms luckperms = LuckPermsProvider.get();
 
     public static boolean modifyPermission(Player player, String permission, boolean value) {
-        PermissionNode node = PermissionNode.builder(permission).value(value).build();
-        User user = luckperms.getUserManager().getUser(player.getUniqueId());
+        final User user = luckperms.getUserManager().getUser(player.getUniqueId());
         if (user == null) {
             return false;
         }
-        DataMutateResult result = user.data().add(node);
-        return result.wasSuccessful();
+
+        final PermissionNode node = PermissionNode.builder(permission).value(value).build();
+        return user.data().add(node).wasSuccessful();
     }
 }
