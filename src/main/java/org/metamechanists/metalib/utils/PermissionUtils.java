11e998c4a04e313f94edf429de7eaad6c6c9b1ef
MetaMechanists <percy.baker.0@gmail.com>
2023-06-06 23:13:57 +0100

    Fix modifyPermission to actually use LP API

--- src/main/java/org/metamechanists/metalib/utils/PermissionUtils.java
+++ src/main/java/org/metamechanists/metalib/utils/PermissionUtils.java
@@ -1,13 +1,23 @@
 package org.metamechanists.metalib.utils;
 
-import org.bukkit.Bukkit;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.data.DataMutateResult;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.types.PermissionNode;
 import org.bukkit.entity.Player;
 
 @SuppressWarnings("unused")
 public class PermissionUtils {
-    public static boolean modifyPermission(Player player, String permission, boolean add) {
-        return Bukkit.dispatchCommand(
-                Bukkit.getConsoleSender(),
-                "lp user " + player.getDisplayName() + " permission set " + permission + " " + add);
+    private static final LuckPerms luckperms = LuckPermsProvider.get();
+
+    public static boolean modifyPermission(Player player, String permission, boolean value) {
+        PermissionNode node = PermissionNode.builder(permission).value(value).build();
+        User user = luckperms.getUserManager().getUser(player.getUniqueId());
+        if (user == null) {
+            return false;
+        }
+        DataMutateResult result = user.data().add(node);
+        return result.wasSuccessful();
     }
 }
