a683e876a0e159d28d72b066c055eb19d13b4709
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-10-11 18:44:01 -0500

    replace with method

--- src/main/java/org/metamechanists/metalib/utils/SlimefunUtils.java
+++ src/main/java/org/metamechanists/metalib/utils/SlimefunUtils.java
@@ -3,13 +3,41 @@ package org.metamechanists.metalib.utils;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
 import org.metamechanists.metalib.MetaLib;
 
+import java.util.List;
+
 public class SlimefunUtils {
+
+    private static SlimefunItemStack errorItemStack(String id) {
+        return new SlimefunItemStack(
+                "MM_ERROR_" + id.toUpperCase(),
+                Material.BARRIER,
+                "&cIncorrect Id: " + id);
+    }
+
+    public static void loadWithPosition(SlimefunItem slimefunItem, int position) {
+        final List<SlimefunItem> items = slimefunItem.getItemGroup().getItems();
+        final RecipeType recipeType = slimefunItem.getRecipeType();
+        final ItemStack[] recipe = slimefunItem.getRecipe();
+        final ItemStack output = slimefunItem.getRecipeOutput();
+
+        if (position < 0 || position > items.size()) {
+            slimefunItem.warn("Position is invalid, appending to the item group normally.");
+            position = items.size() - 1;
+        }
+
+        if (!slimefunItem.isHidden()) {
+            items.add(position, slimefunItem);
+        }
+        recipeType.register(recipe, output);
+    }
 
     public static ItemGroup getItemGroup(String id) {
         final String[] pair = id.split(":");
@@ -21,13 +49,6 @@ public class SlimefunUtils {
             }
         }
         return null;
-    }
-
-    private static SlimefunItemStack errorItemStack(String id) {
-        return new SlimefunItemStack(
-                "MM_ERROR_" + id.toUpperCase(),
-                Material.BARRIER,
-                "&cIncorrect Id: " + id);
     }
 
     public static SlimefunItemStack getSlimefunItem(String id) {
