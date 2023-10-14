b97330fa7c94ae2bae79f9c1d2005163eb15ccfc
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-10-14 17:53:24 -0500

    account for multiple loadWithPosition calls

--- src/main/java/org/metamechanists/metalib/utils/SlimefunUtils.java
+++ src/main/java/org/metamechanists/metalib/utils/SlimefunUtils.java
@@ -5,15 +5,17 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.metamechanists.metalib.MetaLib;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SlimefunUtils {
+
+    private static final Map<ItemGroup, List<Integer>> INDEX_OFFSETS = new HashMap<>();
 
     private static SlimefunItemStack errorItemStack(String id) {
         return new SlimefunItemStack(
@@ -23,10 +25,25 @@ public class SlimefunUtils {
     }
 
     public static void loadWithPosition(SlimefunItem slimefunItem, int position) {
-        final List<SlimefunItem> items = slimefunItem.getItemGroup().getItems();
+        final ItemGroup itemGroup = slimefunItem.getItemGroup();
+        final List<SlimefunItem> items = itemGroup.getItems();
         final RecipeType recipeType = slimefunItem.getRecipeType();
         final ItemStack[] recipe = slimefunItem.getRecipe();
         final ItemStack output = slimefunItem.getRecipeOutput();
+
+        // Calculate Index Difference
+        final List<Integer> offsets = INDEX_OFFSETS.getOrDefault(itemGroup, new ArrayList<>());
+        int difference = 0;
+
+        for (int offset : offsets) {
+            if (offset < position) {
+                difference++;
+            }
+        }
+
+        offsets.add(position);
+        INDEX_OFFSETS.put(itemGroup, offsets);
+        position += difference;
 
         if (position < 0 || position > items.size()) {
             slimefunItem.warn("Position is invalid, appending to the item group normally.");
